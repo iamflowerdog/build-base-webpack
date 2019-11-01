b98e42f0470fa194ce6ba3fdd400b76a974c240a
Mr.YanG <yangyonghai9125@hotmail.com>
2019-11-01 14:13:16 +0800

    chore: add omit files src server

--- test/unit/webpack.base.test.js
+++ test/unit/webpack.base.test.js
@@ -4,7 +4,7 @@ const assert = require('assert');
 describe('webpack.base.js unit case', () => {
     const baseConfig = require('../../lib/webpack.base');
     it('entry', () => {
-        assert.equal(baseConfig.entry.index, '/Users/yang/Code/learn_webpack_geekbang/build-webpack/test/smoke/template/src/index/index.js');
-        assert.equal(baseConfig.entry.search, '/Users/yang/Code/learn_webpack_geekbang/build-webpack/test/smoke/template/src/search/index.js');
+        assert.equal(baseConfig.entry.index.indexOf('/build-base-webpack/test/smoke/template/src/index/index.js') > -1, true);
+        assert.equal(baseConfig.entry.search.indexOf('/build-base-webpack/test/smoke/template/src/search/index.js') > -1, true);
     })
 })
